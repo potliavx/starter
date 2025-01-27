20a374f7afc0621e92acb991b23f73a9272c5bd5
Vijay Potlia <vijay.potlia@outlook.com>
2025-01-26 23:04:04 -0500

    add new files

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  "LazyVim/LazyVim",
+  opts = {
+    colorscheme = "catppuccin-latte",
+  },
+}
+
+-- a couple of other colorschemes I liked:
+--  colorscheme = "catppuccin",
+--  colorscheme = "catppuccin-latte",
+--  colorscheme = "evening"
+--  colorscheme = "murphy"
+--  colorscheme = "shine"
+--  colorscheme = "torte"
+--  colorscheme = "vim"
+--  colorscheme = "zellner"
